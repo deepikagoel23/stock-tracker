cf93980b811a70ab4bc1b64aa28d4373a9cee476
deepikagoel23 <deepika.student.eee24@itbhu.ac.in>
2026-03-06 01:00:01 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,10 +1,15 @@
 import os
-import time
 import requests
 from datetime import datetime
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from webdriver_manager.chrome import ChromeDriverManager
 
 # ================= CONFIG =================
